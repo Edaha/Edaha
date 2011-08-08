3d0e7dc14a108b7eda5d7fb1e248b55926e8967d
N3X15 <nexis@7chan.org>
2011-08-07 18:10:26 -0700

    Add README, re-indent the shit out of everything

--- init.php
+++ init.php
@@ -5,25 +5,24 @@ DEFINE('KX_SCRIPT', KX_ROOT);
 DEFINE('KX_LIB', KX_ROOT . '/application/lib/kx'); // Full path to kx's library files
 DEFINE('KUSABA_RUNNING', true);
 
-require_once(KX_LIB."/../Twig/Autoloader.php");
-Twig_Autoloader::register();
-require_once(KX_LIB . '/kxAutoload.class.php');
-require_once(KX_ROOT . '/application/lib/gettext/gettext.inc.php');
+require_once (KX_LIB . "/../Twig/Autoloader.php");
+Twig_Autoloader :: register();
+require_once (KX_LIB . '/kxAutoload.class.php');
+require_once (KX_ROOT . '/application/lib/gettext/gettext.inc.php');
 
-$repository = kxAutoload::registerRepository(KX_LIB, array('prefix' => 'kx')); // Add the autoloader repository in kx's lib dir, listen only for classes starting with the string 'kx'
+$repository = kxAutoload :: registerRepository(KX_LIB, array (
+    'prefix' => 'kx'
+)); // Add the autoloader repository in kx's lib dir, listen only for classes starting with the string 'kx'
 
-kxEnv::initialize('dev', KX_ROOT .'/config'); // Setup the main environment, make it read config files etc, the lots
-kxEnv::set('kx:autoload:repository:kx:id', $repository); // If we want to unload the kx autoloader at some point, store the id here.
+kxEnv :: initialize('dev', KX_ROOT . '/config'); // Setup the main environment, make it read config files etc, the lots
+kxEnv :: set('kx:autoload:repository:kx:id', $repository); // If we want to unload the kx autoloader at some point, store the id here.
 
-foreach(kxEnv::get('kx:autoload:load') as $repo => $opts) {
-  kxEnv::set(
-    sprintf('kx:autoload:repository:%s:id', $repo),
-    kxAutoload::registerRepository(
-      sprintf('%s/%s/%s', KX_ROOT, 'application/lib', $opts['path']),
-      array('prefix' => $opts['prefix'])));
+foreach (kxEnv :: get('kx:autoload:load') as $repo => $opts) {
+    kxEnv :: set(sprintf('kx:autoload:repository:%s:id', $repo), kxAutoload :: registerRepository(sprintf('%s/%s/%s', KX_ROOT, 'application/lib', $opts['path']), array (
+        'prefix' => $opts['prefix']
+    )));
 }
 
 // Cleanup global namespace
-unset($repository);
-
+unset ($repository);
 ?>

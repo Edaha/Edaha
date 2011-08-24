63292235b29b78c2562a50a69549c63f10a729d1
Sazpaimon <Sazpaimon@73d99b7e-4f53-4972-b7b7-2ecd3c4d89ee>
2011-08-24 15:44:04 +0000

    Made stats section use prepared statements properly.
    git-svn-id: http://kusabax.cultnet.net/svn/repo/branches/1.0@338 73d99b7e-4f53-4972-b7b7-2ecd3c4d89ee

--- application/board/modules/manage/statistics/stats.php
+++ application/board/modules/manage/statistics/stats.php
@@ -3,7 +3,7 @@
 class manage_board_statistics_stats extends kxCmd {
   public function exec(kxEnv $environment){
   
-    $type = Array("posts", "uniques", "files");
+    $types = Array("posts", "uniques", "files");
     if (!isset($this->request['time'])) {
       $this->request['time'] = 24;
     }
@@ -11,36 +11,36 @@ class manage_board_statistics_stats extends kxCmd {
                        ->fields("boards")
                        ->execute()
                        ->fetchAll();
+    foreach ($types as $type) {
+      switch ($type) {
+        case "posts":
+          // Total posts
+          $result = $this->db->select("posts");
+          break;
+        case "uniques":
+          // Total uniques
+          $result = $this->db->select("posts")
+                              ->fields("posts", array("post_ip_md5"))
+                              ->distinct();
+          break;
+        case "files":
+          // Total Files
+          $result = $this->db->select("post_files");
+          $result->join("posts", NULL, "file_post = post_id");
+          break;
+      }
+      $result = $result->where("post_deleted = ?")
+                       ->where("post_board = ?")
+                       ->where("post_timestamp >= ?");
+      $results[$type] = $result->countQuery()
+                                        ->build();
+    }
+
     foreach ($boards as $board) {
-      reset($type);
-      do {
-        switch (current($type)) {
-          case "posts":
-            // Total posts
-            $results = $this->db->select("posts");
-            break;
-          case "uniques":
-            // Total uniques
-            $results = $this->db->select("posts")
-                                ->fields("posts", array("post_ip_md5"))
-                                ->distinct();
-            break;
-          case "files":
-            // Total Files
-            $results = $this->db->select("post_files");
-            $results->join("posts", NULL, "file_post = post_id");
-            break;
-        }
-        $results = $results->condition("post_deleted", 0)
-                           ->condition("post_board", $board->board_id);
-        if (!empty($this->request['time'])) {
-          $results = $results->condition("post_timestamp", time()-( $this->request['time'] * 60 * 60 ), ">=");
-        }
-        $twigData['stats'][$board->board_name][current($type)] = $results->countQuery()
-                                                                ->execute()
-                                                                ->fetchField();
-                                                       
-      } while ( next($type) );
+      foreach ($results as $k=>$result) {
+            $result->execute(array(0, $board->board_id, (!empty($this->request['time']) ? (time()-( $this->request['time'] * 60 * 60 )) : 0)));
+            $twigData['stats'][$board->board_name][$k] = $result->fetchField();
+      }
     }
     kxTemplate::output("manage/stats", $twigData);
   }

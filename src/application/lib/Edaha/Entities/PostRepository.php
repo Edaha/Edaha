ffc6b814fdb4c4578e8693565f63d6cb4bb43b95
harijuku <201527236+harijuku@users.noreply.github.com>
2025-05-02 23:46:17 -0700

    Additional utility Post repository definitions

--- src/application/lib/Edaha/Entities/PostRepository.php
+++ src/application/lib/Edaha/Entities/PostRepository.php
@@ -5,7 +5,20 @@ use Doctrine\ORM\EntityRepository;
 
 class PostRepository extends EntityRepository
 {
-    public function getPaginatedThreadsByBoard($board_id, $page = 1, $limit = 10)
+    public function getAllPaginatedThreads($page = 1, $limit = 10)
+    {
+        $dql = "SELECT p, b
+                FROM \Edaha\Entities\Post p
+                JOIN p.board b
+                WHERE p.parent IS NULL
+                ORDER BY p.stickied_at DESC, p.bumped_at DESC";
+        $query = $this->getEntityManager()->createQuery($dql);
+        $query->setFirstResult(($page - 1) * $limit);
+        $query->setMaxResults($limit);
+        return $query->getResult();
+    }
+
+    public function getBoardPaginatedThreads($board_id, $page = 1, $limit = 10)
     {
         $dql = "SELECT p, b
                 FROM \Edaha\Entities\Post p
@@ -32,7 +45,7 @@ class PostRepository extends EntityRepository
         return $query->getResult();
     }
 
-    public function getRecentPosts(int $board_id, ?int $limit = 10)
+    public function getBoardRecentPosts(int $board_id, ?int $limit = 10)
     {
         $dql = "SELECT p, b
                 FROM \Edaha\Entities\Post p
@@ -47,21 +60,49 @@ class PostRepository extends EntityRepository
         return $query->getResult();
     }
 
-    public function getRecentThreads($boardName, $limit = 10)
+    public function getAllRecentThreads(?int $limit = 10)
     {
         $dql = "SELECT p, b
                 FROM \Edaha\Entities\Post p
                 JOIN p.board b
-                WHERE b.name = :boardName AND p.parent IS NULL
+                WHERE p.parent IS NULL
                 ORDER BY p.created_at DESC";
         $query = $this->getEntityManager()->createQuery($dql);
-        $query->setParameter('boardName', $boardName);
         if (!is_null($limit)) {
             $query->setMaxResults($limit);
         }
         return $query->getResult();
     }
 
+    public function getBoardRecentThreads(int $board_id, ?int $limit = 10)
+    {
+        $dql = "SELECT p, b
+                FROM \Edaha\Entities\Post p
+                JOIN p.board b
+                WHERE b.id = :board_id AND p.parent IS NULL
+                ORDER BY p.created_at DESC";
+        $query = $this->getEntityManager()->createQuery($dql);
+        $query->setParameter('board_id', $board_id);
+        if (!is_null($limit)) {
+            $query->setMaxResults($limit);
+        }
+        return $query->getResult();
+    }
+
+    public function getThreadReplies(int $post_id, ?int $limit = 10)
+    {
+        $dql = "SELECT p, b
+                FROM \Edaha\Entities\Post p
+                JOIN p.board b
+                WHERE p.parent = :post_id
+                ORDER BY p.created_at DESC";
+        $query = $this->getEntityManager()->createQuery($dql);
+        $query->setParameter('post_id', $post_id);
+        if (!is_null($limit)) {
+            $query->setMaxResults($limit);
+        }
+        return $query->getResult();
+    }
 
     public function getPostsByIp($ip, $limit = 10)
     {

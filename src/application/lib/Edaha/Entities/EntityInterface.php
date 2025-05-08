aef55c5618b0408a256dec5afc38ab483bd48a16
harijuku <201527236+harijuku@users.noreply.github.com>
2025-05-07 20:30:36 -0700

    Remove unused class

--- src/application/lib/Edaha/Entities/EntityInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Edaha\Entities;
-
-interface EntityInterface {
-    public static function loadFromDb(array $identifiers, object &$db);
-    public static function loadFromAssoc(array $assoc);
-}

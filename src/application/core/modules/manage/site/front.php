75892a3071a81236126071c913820edf193a1759
harijuku <201527236+harijuku@users.noreply.github.com>
2025-06-01 17:41:15 -0700

    Working implementation of news.php using frontboards

--- src/application/core/modules/manage/site/front.php
+++ src/application/core/modules/manage/site/front.php
@@ -1,154 +1,241 @@
 <?php
+/**
+ * Edaha - Site Management Front Controller
+ *
+ * Handles front page management actions (news, FAQ, rules) for the admin panel.
+ * Supports creating, editing, and deleting entries via the management interface.
+ *
+ * @package   Edaha
+ * @category  Modules
+ * @module    manage_core_site_front
+ */
 
-class manage_core_site_front extends kxCmd {
-  /**
-   * Arguments eventually being sent to twig
-   * 
-   * @var Array()
-   */
-  protected $twigData;
-  
-  public function exec(kxEnv $environment){
-    switch ($this->request['action']) {
-      case 'post':
-        $this->_post();
-        break;
-      case 'edit':
-        $this->_edit();
-        break;
-      case 'del':
-        $this->_del();
-        break;
+use Edaha\Entities\Board;
+use Edaha\Entities\Section;
+use Edaha\Entities\Post;
+
+class manage_core_site_front extends kxCmd
+{
+    /**
+     * Arguments eventually being sent to twig
+     *
+     * @var Array()
+     */
+    protected $twigData;
+
+    protected ?Board $board = null;
+
+    public function exec(kxEnv $environment)
+    {
+        if (isset($this->request['board_id'])) {
+            $this->board = $this->entityManager->getRepository(Board::class)->find($this->request['board_id']);
+            if (! $this->board) {
+                throw new Exception('Board not found');
+            }
+            $this->twigData['board'] = $this->board;
+        }
+
+        switch ($this->request['action']) {
+            case 'post':
+                $this->_post();
+                break;
+            case 'edit':
+                $this->_edit();
+                break;
+            case 'del':
+                $this->_del();
+                break;
+        }
+        switch ($this->request['do']) {
+            case 'news':
+            default:
+                $this->_news();
+                break;
+            case 'faq':
+                $this->_faq();
+                break;
+            case 'rules':
+                $this->_rules();
+                break;
+            case 'frontpage':
+                $this->_frontpage();
+                break;
+            case 'frontpage_manage':
+                $this->_frontpage_manage();
+                break;
+        }
     }
-    switch ($this->request['do']) {        
-      case 'news':
-      default:
-        $this->_news();
-        break;
-      case 'faq':
-        $this->_faq();
-        break;
-      case 'rules':
-        $this->_rules();
-        break;
-    }    
-  }
-  
-  private function _news() {
-    $this->twigData['entries'] = $this->db->select("front")
-                                      ->fields("front")
-                                      ->condition("entry_type", 0)
-                                      ->orderBy("entry_time", "DESC")
-                                      ->execute()
-                                      ->fetchAll();
-    kxTemplate::output("manage/news", $this->twigData);
-  }
-  
-  private function _faq() {
-    $this->twigData['entries'] = $this->db->select("front")
-                                      ->fields("front")
-                                      ->condition("entry_type", 1)
-                                      ->orderBy("entry_order", "ASC")
-                                      ->execute()
-                                      ->fetchAll();
-    kxTemplate::output("manage/faq", $this->twigData);
-  }
-  
-  private function _rules() {
-    $this->twigData['entries'] = $this->db->select("front")
-                                      ->fields("front")
-                                      ->condition("entry_type", 2)
-                                      ->orderBy("entry_order", "ASC")
-                                      ->execute()
-                                      ->fetchAll();
-    kxTemplate::output("manage/rules", $this->twigData);
-  }  
 
-  private function _post() {
-    // Handles posting of front page content
-    kxForm::addRule('subject','required')
-          ->addRule('message','required')
-          ->addRule('type','numeric')
-          ->check();
-    $fields = array(
-              'entry_subject' => $this->request['subject'],
-              'entry_message' => $this->request['message'],
-              'entry_type'    => intval($this->request['type'])
-              );
+    private function __install()
+    {
+        $frontpage_section = $this->entityManager->getRepository(Section::class)->findOneBy(['name' => 'Frontpage']);
+        if (! $frontpage_section) {
+            $frontpage_section = new Section('Frontpage', false);
+            $this->entityManager->persist($frontpage_section);
 
-    if ($this->request['do'] == 'news') {
-      // News-specific fields
-      $fields['entry_email'] = $this->request['email'];
-      $fields['entry_name'] = ''; //TODO: make entry_name for current username
-      
-      if ($this->request['edit'] == "") {
-        $fields['entry_time'] = time();
+            // TODO Flexible Frontpage Boards
+            $news_board = $this->entityManager->getRepository(Board::class)->findOneBy(['name' => 'News']);
+            if (! $news_board) {
+                $news_board = new Board('News', 'frontpage_news');
+                $this->entityManager->persist($news_board);
+            }
+            $news_board->is_locked = true;
 
+            $faq_board = $this->entityManager->getRepository(Board::class)->findOneBy(['name' => 'FAQ']);
+            if (! $faq_board) {
+                $faq_board = new Board('FAQ', 'frontpage_faq');
+                $this->entityManager->persist($faq_board);
+            }
+            $faq_board->is_locked = true;
 
-      }
-    } else {
-      // Other front page fields
-      $fields['entry_order'] = $this->request['order'];
-	if ($this->request['order'] == "") {
-		$fields['entry_order'] = 0;
-	}
+            $rules_board = $this->entityManager->getRepository(Board::class)->findOneBy(['name' => 'Rules']);
+            if (! $rules_board) {
+                $rules_board = new Board('Rules', 'frontpage_rules');
+                $this->entityManager->persist($rules_board);
+            }
+            $rules_board->is_locked = true;
+
+            $frontpage_section->addBoard($news_board);
+            $frontpage_section->addBoard($faq_board);
+            $frontpage_section->addBoard($rules_board);
+
+            $this->entityManager->flush();
+        }
     }
-    
-    if ($this->request['edit'] == "") {
-      // New post
-      $this->db->insert("front")
-               ->fields($fields)
-               ->execute();
-      $this->twigData['notice'] = _('Entry successfully added.');
-      logging::addLogEntry(
-        kxFunc::getManageUser()['user_name'],
-        sprintf('Created new %s entry', $this->request['do']),
-        __CLASS__
-      );
-    } else {
-      // Update post
-      $this->db->update("front")
-               ->fields($fields)
-               ->condition("entry_id", $this->request['edit'])
-               ->execute();
-      $this->twigData['notice'] = _('Entry successfully edited.');
-      logging::addLogEntry(
-        kxFunc::getManageUser()['user_name'],
-        sprintf('Edited %s entry', $this->request['do']),
-        __CLASS__
-      );
+
+    private function _frontpage()
+    {
+        $frontpage_section = $this->entityManager->getRepository(Section::class)->findOneBy(['name' => 'Frontpage']);
+        if (! $frontpage_section) {
+            $this->__install();
+            $frontpage_section = $this->entityManager->getRepository(Section::class)->findOneBy(['name' => 'Frontpage']);
+        }
+
+        $this->twigData['frontpage_section'] = $frontpage_section;
+
+        kxTemplate::output("manage/frontpage", $this->twigData);
     }
-    $this->twigData['notice_type'] = 'success';
-  }
-  
-  private function _edit() {
-    if ($this->request['do'] == 'news') {
-      $type = 0;
-    } else if ($this->request['do'] == 'faq') {
-      $type = 1;
-    } else if ($this->request['do'] == 'rules') {
-      $type = 2;
+
+    private function _frontpage_manage()
+    {
+        kxTemplate::output("manage/frontpage_manage", $this->twigData);
     }
-    $this->twigData['entry'] = $this->db->select("front")
-                                        ->fields("front")
-                                        ->condition("entry_type", $type)
-                                        ->condition("entry_id", $this->request['id'])
-                                        ->orderBy("entry_id", "DESC")
-                                        ->execute()
-                                        ->fetchAssoc();
-  }
-  
-  private function _del() {
-    $this->db->delete("front")
-             ->condition("entry_id", $this->request['id'])
-             ->execute();
-    $this->twigData['notice_type'] = 'success';
-    $this->twigData['notice'] = _('Entry successfully deleted.');
-    logging::addLogEntry(
-      kxFunc::getManageUser()['user_name'],
-      sprintf('Deleted %s entry', $this->request['do']),
-      __CLASS__
-    );
-  }
+
+    private function _news()
+    {
+        $this->twigData['entries'] = $this->db->select("front")
+            ->fields("front")
+            ->condition("entry_type", 0)
+            ->orderBy("entry_time", "DESC")
+            ->execute()
+            ->fetchAll();
+        kxTemplate::output("manage/news", $this->twigData);
+    }
+
+    private function _faq()
+    {
+        $this->twigData['entries'] = $this->db->select("front")
+            ->fields("front")
+            ->condition("entry_type", 1)
+            ->orderBy("entry_order", "ASC")
+            ->execute()
+            ->fetchAll();
+        kxTemplate::output("manage/faq", $this->twigData);
+    }
+
+    private function _rules()
+    {
+        $this->twigData['entries'] = $this->db->select("front")
+            ->fields("front")
+            ->condition("entry_type", 2)
+            ->orderBy("entry_order", "ASC")
+            ->execute()
+            ->fetchAll();
+        kxTemplate::output("manage/rules", $this->twigData);
+    }
+
+    private function _post()
+    {
+        // Handles posting of front page content
+        kxForm::addRule('subject', 'required')
+            ->addRule('message', 'required')
+            ->addRule('board_id', 'numeric')
+            ->check();
+
+        if (isset($this->request['edit']) && $this->request['edit'] != '') {
+            $post = $this->entityManager->getRepository(Post::class)->find($this->request['edit']);
+            if (! $post) {
+                throw new Exception('Post not found');
+            }
+        } else {
+            $post = New Post(
+                $this->board,
+                $this->request['message'],
+                $this->request['subject']
+            );
+        }
+        
+        $post->poster->name = kxFunc::getManageUser()['user_name'];
+
+        if (isset($this->request['email'])) {
+            $post->poster->email = $this->request['email'];
+        }
+
+        // TODO Post ordering
+        
+        $this->entityManager->persist($post);
+        $this->entityManager->flush();
+
+        if ($this->request['edit'] == "") {
+            // New post
+            $this->twigData['notice'] = _('Entry successfully added.');
+            logging::addLogEntry(
+                kxFunc::getManageUser()['user_name'],
+                sprintf('Created new post on %s', $this->board->name),
+                __CLASS__
+            );
+        } else {
+            // Update post
+            $this->twigData['notice'] = _('Entry successfully edited.');
+            logging::addLogEntry(
+                kxFunc::getManageUser()['user_name'],
+                sprintf('Edited entry post %s on %s', $post->id, $this->board->name),
+                __CLASS__
+            );
+        }
+        $this->twigData['notice_type'] = 'success';
+    }
+
+    private function _edit()
+    {
+        if ($this->request['do'] == 'news') {
+            $type = 0;
+        } else if ($this->request['do'] == 'faq') {
+            $type = 1;
+        } else if ($this->request['do'] == 'rules') {
+            $type = 2;
+        }
+        $this->twigData['entry'] = $this->db->select("front")
+            ->fields("front")
+            ->condition("entry_type", $type)
+            ->condition("entry_id", $this->request['id'])
+            ->orderBy("entry_id", "DESC")
+            ->execute()
+            ->fetchAssoc();
+    }
+
+    private function _del()
+    {
+        $this->db->delete("front")
+            ->condition("entry_id", $this->request['id'])
+            ->execute();
+        $this->twigData['notice_type'] = 'success';
+        $this->twigData['notice']      = _('Entry successfully deleted.');
+        logging::addLogEntry(
+            kxFunc::getManageUser()['user_name'],
+            sprintf('Deleted %s entry', $this->request['do']),
+            __CLASS__
+        );
+    }
 
 }

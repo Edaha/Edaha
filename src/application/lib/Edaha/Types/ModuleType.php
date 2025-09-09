2b70916b2f9aa2626bab433d9ef4b22f66ef7574
harijuku <201527236+harijuku@users.noreply.github.com>
2025-09-08 19:49:28 -0700

    Additional module types

--- src/application/lib/Edaha/Types/ModuleType.php
+++ src/application/lib/Edaha/Types/ModuleType.php
@@ -5,6 +5,8 @@ enum ModuleType: string
 {
     case Core = 'core';
     case BoardType = 'board';
+    case IndexType = 'index';
+    case SiteType = 'site';
     case BoardRegenerator = 'board_regenerator';
     case BoardRenderer = 'board_renderer';
     case PostPreProcessor = 'post_preprocessor';

300110d88d10228cd560299f143850fe553bd8b8
harijuku <201527236+harijuku@users.noreply.github.com>
2026-07-30 20:47:29 -0700

    Split kxCmd, kxCmd_default, kxCmdResolv

---
+++ src/application/lib/kx/kxCmd/kxCmd.class.php
@@ -0,0 +1,68 @@
+<?php
+
+abstract class kxCmd
+{
+    /**
+     * Environment Shortcuts.
+     *
+     * @var object
+     */
+    protected $environment;
+
+    /**
+     * kxDB instance.
+     *
+     * @var object
+     */
+    protected $db;
+
+    /**
+     * kxOrm instance.
+     *
+     * @var object
+     */
+    protected $entityManager;
+
+    /**
+     * The request infortmation.
+     *
+     * @var object
+     */
+    protected $request;
+
+    /**
+     * Constructor.
+     */
+    final public function __construct() {}
+
+    /**
+     * Make shortcuts for kxEnv and kxDB.
+     *
+     * @param	object	kxEnv reference
+     */
+    public function makeRegistryShortcuts(kxEnv $environment)
+    {
+        $this->environment = $environment;
+        $this->db = kxDB::getinstance();
+        $this->request = kxEnv::$request;
+        $this->entityManager = kxOrm::getEntityManager();
+    }
+
+    /**
+     * Wrapper for makeRegistryShortcuts() and exec().
+     *
+     * @param	object	kxEnv reference
+     */
+    public function execute(kxEnv $environment)
+    {
+        $this->makeRegistryShortcuts($environment);
+        $this->exec($environment);
+    }
+
+    /**
+     * Do execute method (must be overriden).
+     *
+     * @param	object	kxEnv reference
+     */
+    abstract protected function exec(kxEnv $environment);
+}
---
+++ src/application/lib/kx/kxCmd/kxCmd_default.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * kxCmd_default
+ * For if we don't have a valid command, just load the index.
+ */
+class kxCmd_default extends kxCmd
+{
+    /**
+     * Do execute method.
+     *
+     * @param	object	kxCmd reference
+     */
+    protected function exec(kxEnv $environment)
+    {
+        @header('Location: '.kxEnv::Get('kx:paths:main:path').kxEnv::Get('kx:paths:main:folder'));
+    }
+}

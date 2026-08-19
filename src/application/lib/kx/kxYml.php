7b0dbd15e4beafd48a2750162e75da05bc762186
harijuku <201527236+harijuku@users.noreply.github.com>
2026-08-18 22:23:13 -0700

    docs

--- src/application/lib/kx/kxYml.php
+++ src/application/lib/kx/kxYml.php
@@ -2,35 +2,47 @@
 
 namespace kx;
 
-use \Spyc;
+use Spyc;
 
 class kxYml
 {
-    private static $useSpyc = true;
-    private static $init = false;
+    private static bool $useSpyc = true;
+    private static bool $init = false;
 
-    public static function loadFile($file)
+    /**
+     * Takes a $path to a .yml file and returns an array representation of it.
+     */
+    public static function loadFile(string $path): array
     {
         self::_init();
 
-        return self::$useSpyc ? Spyc::YAMLLoad($file) : syck_load(file_get_contents($file));
+        return self::$useSpyc ? \Spyc::YAMLLoad($path) : syck_load(file_get_contents($path));
     }
 
-    public static function loadString($string)
+    /**
+     * Takes a $string representation of a YAML document and returns an array representation of it.
+     */
+    public static function loadString(string $string): array
     {
         self::_init();
 
-        return self::$useSpyc ? Spyc::YAMLLoadString($string) : syck_load($string);
+        return self::$useSpyc ? \Spyc::YAMLLoadString($string) : syck_load($string);
     }
 
-    public static function dump($array)
+    /**
+     * Take an associative $array and return a string containing its YAML representation.
+     */
+    public static function dump(array $array): string
     {
         self::_init();
 
-        return self::$useSpyc ? Spyc::YAMLDump($array) : syck_dump($array);
+        return self::$useSpyc ? \Spyc::YAMLDump($array) : syck_dump($array);
     }
 
-    private static function _init()
+    /**
+     * Sets up the class, checks if syck is installed, otherwise uses Spyc.
+     */
+    private static function _init(): bool
     {
         if (!self::$init) {
             if (function_exists('syck_load')) {
